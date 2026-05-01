424766092fb9d3781497cd3bb498bb2e93bbdfdc
gael-najera <gaelnajeradl@gmail.com>
2026-04-30 23:47:57 -0600

    Hola

---
+++ Ximenita/src/main/java/com/gael/Ximenita/XimenitaApplication.java
@@ -0,0 +1,13 @@
+package com.gael.Ximenita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XimenitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XimenitaApplication.class, args);
+	}
+
+}
---
+++ Ximenita/src/test/java/com/gael/Ximenita/XimenitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gael.Ximenita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XimenitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
